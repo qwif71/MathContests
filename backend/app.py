ebe4ed19e34095ae4f85caec8b54c6ee16008d4b
Bella Li <bellali@Aidens-MacBook-Pro.local>
2026-06-18 23:51:41 -0500

    Fix CORS to allow credentials from local file:// testing

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["null"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
